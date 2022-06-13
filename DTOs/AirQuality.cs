5769105421249e453cd25104d69c681cbab7e9ed
Ranjam al <ranjam.alee@ineo.fi>
2022-06-13 13:08:08 +0300

    fetch data from openaq

---
+++ DTOs/AirQuality.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BlazorAirQuality.DTOs
+{
+    public partial class AirQuality
+    {
+        public string? Code { get; set; }
+
+        public string? Name { get; set; }
+        public int Locations { get; set; }
+
+        public long Count { get; set; }
+
+        public DateTime LastUpdated { get; set; }
+    }
+}
---
+++ DTOs/Response.cs
@@ -0,0 +1,24 @@
+﻿namespace BlazorAirQuality.DTOs
+{
+    public partial class Response
+    {
+        public Meta? Meta { get; set; }
+
+        public AirQuality[]? Results { get; set; }
+    }
+
+    public partial class Meta
+    {
+        public string? Name { get; set; }
+
+        public string? License { get; set; }
+
+        public string? Website { get; set; }
+
+        public long Page { get; set; }
+
+        public long Limit { get; set; }
+
+        public long Found { get; set; }
+    }
+}
